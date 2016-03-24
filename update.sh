2c5e333ace0266804e18fd8314a0f6929e2925f3
Charles Julian Knight <charles@rabidaudio.com>
2016-03-24 14:45:14 -0400

    updated script order

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 VERSION="$1"
-git submodule update --remote
-cd Parse-SDK-JS
 if [ -z "$VERSION" ]; then
 	echo "Please supply a version, e.g."
 	echo "    ./update.sh v1.8.0"
 	exit 1
 fi
+git submodule update --remote
+cd Parse-SDK-JS
 git checkout ${VERSION}
 npm install
 ./build_releases.sh
