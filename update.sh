8a713e38bcdd3f5e66e8a6a230f84e68fa216952
Charles Julian Knight <charles@rabidaudio.com>
2016-03-24 14:38:11 -0400

    updated update script for full update process

--- update.sh
+++ update.sh
@@ -3,12 +3,16 @@ VERSION="$1"
 git submodule update --remote
 cd Parse-SDK-JS
 if [ -z "$VERSION" ]; then
-	git checkout master	
-else
-	git checkout ${VERSION}
+	echo "Please supply a version, e.g."
+	echo "    ./update.sh v1.8.0"
+	exit 1
 fi
+git checkout ${VERSION}
 npm install
 ./build_releases.sh
 cd ..
 cp ./Parse-SDK-JS/dist/parse-latest.js ./parse.js
 cp ./Parse-SDK-JS/dist/parse-latest.min.js ./parse.min.js
+cd ..
+git commit -a -m "updated to $VERSION"
+bower version "$VERSION"
