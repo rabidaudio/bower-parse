ec7c00e804f6463a21fba35b4fb54f2ce1eab5fc
Charles Julian Knight <julian@fixdapp.com>
2020-03-30 21:13:41 -0400

    fix for update script

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 VERSION="$1"
-MESSAGE="$2"
 if [ -z "$VERSION" ]; then
 	echo "Please supply a version, e.g."
 	echo "    ./update.sh 1.8.0"
@@ -15,8 +14,8 @@ npm run build
 cd ..
 cp ./Parse-SDK-JS/dist/* .
 head parse.js | grep "$VERSION" || exit 1
-git commit -a -m "updated to $VERSION \n $MESSAGE"
 cd Parse-SDK-JS
 git reset --hard HEAD
 cd ..
+git commit -a -m "updated to $VERSION"
 bower version "$VERSION"
